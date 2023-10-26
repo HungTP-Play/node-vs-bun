1df250bf3ad5677614074be3bf36a47e8a69e91f
Hung Tran <hung.p.tran@opswat.com>
2023-10-26 14:25:55 +0700

    feat: add DI bun

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,1 +1,10 @@
-console.log('HELLO FROM BUN')
+import Koa from 'koa';
+import { TYPES, nvbContainer } from './container';
+import { Logger } from './logger';
+
+const logger = nvbContainer.get<Logger>(TYPES.Logger);
+
+const app = new Koa();
+
+logger.log(`APP IS RUNNING ON PORT 3333`);
+app.listen(3333);
